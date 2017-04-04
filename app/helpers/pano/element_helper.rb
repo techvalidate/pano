72309eaeb2cb4be9624bd21b6c809bd4dece74f3
Matt Lindeboom <mattl@surveymonkey.com>
2017-04-04 13:01:37 -0700

    fix for arguments

--- app/helpers/pano/element_helper.rb
+++ app/helpers/pano/element_helper.rb
@@ -1,14 +1,16 @@
-module ElementHelper
-  def area_chart(options={})
-    s "<donut-chart #{convert_options_to_data_attributes options}/>"
-  end
+module Pano
+  module ElementHelper
+    def area_chart(options={})
+      s "<donut-chart #{convert_options_to_data_attributes options}/>"
+    end
 
-  def count_up_number(options={}, html_options)
-    content_tag 'count-up-number', convert_options_to_data_attributes(options), html_options
-  end
+    def count_up_number(options, html_options={})
+      content_tag 'count-up-number', convert_options_to_data_attributes(options), html_options
+    end
 
-  def donut_chart(options={})
-    s "<donut-chart #{convert_options_to_data_attributes options} />"
-  end
+    def donut_chart(options={})
+      s "<donut-chart #{convert_options_to_data_attributes options} />"
+    end
 
+  end
 end
