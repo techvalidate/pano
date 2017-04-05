65f761db6f48167670b3a8933782700557ab4e63
Matt Lindeboom <mattl@surveymonkey.com>
2017-04-05 11:07:37 -0700

    Pass string to render rather than block

--- app/helpers/pano/element_helper.rb
+++ app/helpers/pano/element_helper.rb
@@ -1,7 +1,9 @@
 module Pano
   module ElementHelper
     def element(element, html_options = {}, &block)
-      element.render(html_options, &block)
+      block_content = ''
+      block_content += capture(&block) if block_given?
+      element.render(html_options, block_content)
     end
   end
 end
