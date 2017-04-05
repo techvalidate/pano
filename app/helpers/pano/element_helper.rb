0b96d195d74c928c39704a8bdc29bb6b72c65b78
Matt Lindeboom <mattl@surveymonkey.com>
2017-04-05 12:05:35 -0700

    Renaming to be clearer

--- app/helpers/pano/element_helper.rb
+++ app/helpers/pano/element_helper.rb
@@ -1,9 +1,9 @@
 module Pano
   module ElementHelper
-    def element(element, html_options = {}, &block)
+    def element(redux_element, html_options = {}, &block)
       block_content = ''
       block_content += capture(&block) if block_given?
-      element.render(html_options, block_content)
+      redux_element.render(html_options, block_content)
     end
   end
 end
