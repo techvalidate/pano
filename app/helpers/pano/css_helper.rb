41ccd77c64d10ad979cf7f60f8032091a2859fae
Jonathon Wolfe <jon@jonwolfe.com>
2017-05-12 11:55:54 -0700

    start cleanup of pano global css. implement new clors and typography from styleguide

--- app/helpers/pano/css_helper.rb
+++ app/helpers/pano/css_helper.rb
@@ -1,5 +1,6 @@
 module Pano
   module CSSHelper
+
     # this is useful for cases where you want to make sure a specific css class
     # gets added to an options hash, but also allow extra css classes to be included
     def options_plus_class(options, css_class)
@@ -10,5 +11,18 @@ module Pano
         options.merge!(class: css_class)
       end
     end
+
+    def color_swatch(color_name)
+      s "<div class='color-swatch'><span class='color #{color_name}'>&#9679;</span><span class='label'>#{color_name}</span></div>"
+    end
+
+    def trademark
+      s "<span class='trademark'>TM</span>"
+    end
+
+    def circle_r
+      s "<span class='trademark'>&reg;</span>"
+    end
+
   end
 end
