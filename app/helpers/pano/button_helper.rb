c168a2a7c1cca551f39d135ce1ec6f07450b5157
Jonathon Wolfe <jon@jonwolfe.com>
2017-05-24 12:24:08 -0700

    update a couple examples on buttons page

--- app/helpers/pano/button_helper.rb
+++ app/helpers/pano/button_helper.rb
@@ -13,6 +13,10 @@ module Pano
     # button sizes
     # ------------------------------------------------------------
 
+    def huge_btn_to(label, options = {}, html_options = {})
+      btn_to label, options, html_options.add_class('btn-huge')
+    end
+
     def large_btn_to(label, options = {}, html_options = {})
       btn_to label, options, html_options.add_class('btn-large')
     end
@@ -26,43 +30,11 @@ module Pano
     end
 
     # ------------------------------------------------------------
-    # button colors
-    # ------------------------------------------------------------
-
-    def primary_btn_to(label, options = {}, html_options = {})
-      btn_to label, options, html_options.add_class('btn-primary')
-    end
-
-    # ------------------------------------------------------------
     # buttons with icons
     # ------------------------------------------------------------
 
     def icon_btn_to(icon_name, label, options = {}, html_options = {})
       btn_to safe_join([icon(icon_name), label]), options, html_options
-    end
-
-    # ------------------------------------------------------------
-    # combination and other buttons
-    # ------------------------------------------------------------
-
-    def small_primary_btn_to(label, options = {}, html_options = {})
-      btn_to label, options, html_options.add_class('btn-small btn-primary')
-    end
-
-    def small_icon_btn_to(icon_name, label, options = {}, html_options = {})
-      small_btn_to safe_join([icon(icon_name), label]), options, html_options
-    end
-
-    def primary_icon_btn_to(icon_name, label, options = {}, html_options = {})
-      primary_btn_to safe_join([icon(icon_name), label]), options, html_options
-    end
-
-    def small_primary_icon_btn_to(icon_name, label, options = {}, html_options = {})
-      small_primary_btn_to safe_join([icon(icon_name), label]), options, html_options
-    end
-
-    def large_primary_btn_to(label, options = {}, html_options = {})
-      btn_to label, options, html_options.add_class('btn-large btn-primary')
     end
 
   end
