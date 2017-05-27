d1fea5aa7357777076563dccc3527d3eff449e17
Matt Lindeboom <mattl@surveymonkey.com>
2017-05-26 18:34:12 -0700

    Revert "Adds icon_btn, btn-white, btn-gray"
    This reverts commit 7b9ddc4ccb453933ce01376b2291924bf397f834.

--- app/helpers/pano/button_helper.rb
+++ app/helpers/pano/button_helper.rb
@@ -13,11 +13,11 @@ module Pano
     # button sizes
     # ------------------------------------------------------------
 
-    def huge_btn_to(label, options= {}, html_options = {})
-			btn_to label, options, html_options.add_class('btn-huge')
-		end
+    def huge_btn_to(label, options = {}, html_options = {})
+      btn_to label, options, html_options.add_class('btn-huge')
+    end
 
-		def large_btn_to(label, options = {}, html_options = {})
+    def large_btn_to(label, options = {}, html_options = {})
       btn_to label, options, html_options.add_class('btn-large')
     end
 
@@ -28,23 +28,15 @@ module Pano
     def tiny_btn_to(label, options = {}, html_options = {})
       btn_to label, options, html_options.add_class('btn-tiny')
     end
-   
-	 	# ------------------------------------------------------------
+
+    # ------------------------------------------------------------
     # buttons with icons
     # ------------------------------------------------------------
-		
-		def icon_btn(icon_name, options = {}, html_options = {})
-			link_to icon(icon_name), options, html_options.add_class('btn icon-only')
-		end
 
     def icon_btn_to(icon_name, label, options = {}, html_options = {})
-			if html_options[:right] 
-				btn_to safe_join([label, icon(icon_name)]), options, html_options.add_class('icon-rt')
-			else
-				btn_to safe_join([icon(icon_name), label]),  options, html_options
-			end
-  	end
- 
+      btn_to safe_join([icon(icon_name), label]), options, html_options
+    end
+
   end
 
 end
