67328fbfeeedcf786d79a66507b68fd8277363b0
Matt Lindeboom <mattl@surveymonkey.com>
2017-05-30 14:41:57 -0700

    Fixes buttons helper

--- app/helpers/pano/button_helper.rb
+++ app/helpers/pano/button_helper.rb
@@ -13,7 +13,7 @@ module Pano
     # button sizes
     # ------------------------------------------------------------
 
-    def huge_btn_to(label, options = {}, html_options = {})
+    def huge_btn_to(label, options= {}, html_options = {})
       btn_to label, options, html_options.add_class('btn-huge')
     end
 
@@ -33,8 +33,16 @@ module Pano
     # buttons with icons
     # ------------------------------------------------------------
 
+    def icon_btn(icon_name, options = {}, html_options = {})
+      link_to icon(icon_name), options, html_options.add_class('btn icon-only')
+    end
+
     def icon_btn_to(icon_name, label, options = {}, html_options = {})
-      btn_to safe_join([icon(icon_name), label]), options, html_options
+      if html_options[:right] 
+        btn_to safe_join([label, icon(icon_name)]), options, html_options.add_class('icon-rt')
+      else
+        btn_to safe_join([icon(icon_name), label]),  options, html_options
+      end
     end
 
   end
