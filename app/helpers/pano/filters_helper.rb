26c176e83d44254b5a096eb43ea4a8962ff42e16
Jordan McKible <jordan@mckible.com>
2017-06-06 10:28:19 -0700

    Forgot Pano module scoping

--- app/helpers/pano/filters_helper.rb
+++ app/helpers/pano/filters_helper.rb
@@ -1,13 +1,15 @@
-module FiltersHelper
+module Pano
+  module FiltersHelper
 
-  def clear_filters_link(url, remote: false)
-    icon_to 'close', 'Clear All', url, class: 'delete', title: 'Clear all selected filters', remote: remote
+    def clear_filters_link(url, remote: false)
+      icon_to 'close', 'Clear All', url, class: 'delete', title: 'Clear all selected filters', remote: remote
+    end
+
+    def list_filters(filters, remote: false)
+      safe_join(filters.collect do |filter|
+        s_link_to "#{h filter.to_label} #{icon 'close'}", filter.url, class: 'active-filter', remote: remote
+      end)
+    end
+
   end
-
-  def list_filters(filters, remote: false)
-    safe_join(filters.collect do |filter|
-      s_link_to "#{h filter.to_label} #{icon 'close'}", filter.url, class: 'active-filter', remote: remote
-    end)
-  end
-
 end
