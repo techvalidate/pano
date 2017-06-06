12d2a3dc55fddbd46d3b516e2b2ae22fc38e1e81
Jordan McKible <jordan@mckible.com>
2017-06-06 10:25:39 -0700

    Add Filter - used by searches

---
+++ app/models/pano/filter.rb
@@ -0,0 +1,23 @@
+module Pano
+  class Filter
+
+    attr_accessor :dimension, :selected, :url
+
+    def initialize(dimension, selected, url)
+      self.dimension = dimension
+      self.selected  = selected
+      self.url       = url
+    end
+
+    def to_label
+      return "#{dimension}: #{selected}" if dimension == 'Sort By'
+      return "#{dimension}: #{selected.truncate(60)}" if selected.is_a?(String)
+      if selected.size > 2
+        "#{dimension}: #{selected.size} Selected"
+      else
+        "#{dimension}: #{selected.to_sentence(words_connector: ' or ', two_words_connector: ' or ', last_word_connector: ' or ').truncate(60)}"
+      end
+    end
+
+  end
+end
