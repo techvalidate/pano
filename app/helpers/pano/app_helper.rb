eb76a122284d05ca857cf471d12191f28f5cca15
Jonathon Wolfe <jon@jonwolfe.com>
2017-06-22 14:29:13 -0700

    move location of San Francisco fonts for the styleguide pages

--- app/helpers/pano/app_helper.rb
+++ app/helpers/pano/app_helper.rb
@@ -3,14 +3,14 @@ module Pano
 
     def centered_app_page_header(header_data)
       div_tag class: 'centered-page-header' do
-        output = ''
-        output += s(header_data[:back_link]) if header_data[:back_link]
-        output += s(header_data[:right_button]) if header_data[:right_button]
-        output += s(image_tag(header_data[:icon], class: 'header-icon')) if header_data[:icon]
-        output += s(h1_tag(header_data[:title]))
-        output += s(p_tag(header_data[:subtitle], class: 'intro')) if header_data[:subtitle]
-        output += s(p_tag(header_data[:button])) if header_data[:button]
-        s output
+        content = []
+        content << header_data[:back_link] if header_data[:back_link]
+        content << header_data[:right_button] if header_data[:right_button]
+        content << image_tag(header_data[:icon], class: 'header-icon') if header_data[:icon]
+        content << h1_tag(header_data[:title])
+        content << p_tag(header_data[:subtitle], class: 'intro') if header_data[:subtitle]
+        content << p_tag(header_data[:button]) if header_data[:button]
+        safe_join content
       end
     end
 
