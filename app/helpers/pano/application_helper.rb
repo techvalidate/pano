ca7ad9ea519d044044fe620e66b01260a2b2e7db
Matt Miller <mattm@techvalidate.com>
2017-06-22 13:56:32 -0400

    Put back demo account helpers

--- app/helpers/pano/application_helper.rb
+++ app/helpers/pano/application_helper.rb
@@ -1,6 +1,17 @@
 module Pano
   module ApplicationHelper
 
+    def absolute_root_url
+      root_url(host: host_without_demo)
+    end
+
+    def host_without_demo
+      request.host.gsub(/^demo./, '')
+    end
+
+    def host_with_demo
+      'demo.' + host_without_demo
+    end
 
   end
 end
