25a8b01bec1d1e651f5f5e7e7ef0f34eee944e57
Jonathon Wolfe <jon@jonwolfe.com>
2017-06-28 11:45:17 -0700

    schema merge

--- spec/test_app/db/schema.rb
+++ spec/test_app/db/schema.rb
@@ -13,9 +13,9 @@
 ActiveRecord::Schema.define(version: 20170608005159) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
