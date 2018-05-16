e90f5d6aceadf254acc04bc4702550a31d8ade86
Matt Lindeboom <mattl@surveymonkey.com>
2018-05-16 16:44:55 -0700

    simplifies icons

--- app/helpers/pano/icon_helper.rb
+++ app/helpers/pano/icon_helper.rb
@@ -13,18 +13,12 @@ module Pano
     # render an icon
     #--------------------
     def icon(icon_name, options = {})
-
       klass = options[:class] ? (options.delete :class) : ''
       klass += ' material-icons'
       content_tag :i, material_icon_for(icon_name), options.add_class(klass)
     end
 
-    def svg_icon(icon_name, options = {})
-      futuro_icon = futuro_icon_for(icon_name)
-      size = options.delete :size || '32'
 
-      image_tag futuro_icon, options.merge({size: size})
-    end
 
     def med_icon(icon_name, options = {})
       icon icon_name, options.add_class('med-icon')
@@ -45,58 +39,39 @@ module Pano
       i.to_s
     end
 
-    def futuro_icon_for(i)
-      return FuturoIcons[i.to_sym] if FuturoIcons.key? i.to_sym
-      false
+    #--------------------
+    # render an svg icon
+    #--------------------
+
+    def svg_icon(icon_name, options = {})
+      futuro_icon = futuro_icon_for(icon_name)
+      size = options.delete :size
+      options.add_class('svg-icon')
+      options.merge({size: size}) unless size.nil?
+
+      image_tag "pano/icons/#{futuro_icon}.svg", options
     end
 
-    icon_path = 'pano/icons/'
+    def small_svg_icon(icon_name, options = {})
+      options.add_class('svg-icon-small')
+      svg_icon(icon_name, options)
+    end
 
-    FuturoIcons = {
-      'back':                     icon_path + 'arrow.svg',
-      'benchmarks':               icon_path + 'benchmarks.svg',
-      'custom_attributes':        icon_path + 'custom-attributes.svg',
-      'custom_filters':           icon_path + 'custom-filters.svg',
-      'demo':                     icon_path + 'demo.svg',
-      'enhanced':                 icon_path + 'enhanced.svg',
-      'essential':                icon_path + 'essential.svg',
-      'import_file':              icon_path + 'import.svg',
-      'integrations':             icon_path + 'integrations.svg',
-      'knowledge_base':           icon_path + 'knowledge-base.svg',
-      'lists':                    icon_path + 'lists.svg',
-      'organization':             icon_path + 'organization.svg',
-      'password':                 icon_path + 'password.svg',
-      'radar':                    icon_path + 'radar.svg',
-      'settings':                 icon_path + 'settings.svg',
-      'starter':                  icon_path + 'starter.svg',
-      'suppressions':             icon_path + 'suppressions.svg',
-      'large_survey':             icon_path + 'survey.svg',
-      'tags':                     icon_path + 'tags.svg',
-      'team':                     icon_path + 'team.svg',
-      'you':                      icon_path + 'you.svg',
-      'care_about_people':        icon_path + 'care-about-people.svg',
-      'focus_group':              icon_path + 'focus-group.svg',
-      'future_possibility':       icon_path + 'future-possibility.svg',
-      'goals_achieve':            icon_path + 'goals-achieve.svg',
-      'long_term_vision':         icon_path + 'long-term-vision.svg',
-      'loudspeaker_promotion':    icon_path + 'loudspeaker-promotion.svg',
-      'manager':                  icon_path + 'manager.svg',
-      'market_forecast':          icon_path + 'market-forecast.svg',
-      'math_formula':             icon_path + 'math-formula.svg',
-      'mission_accomplished':     icon_path + 'mission-accomplished.svg',
-      'monitoring_project_data':  icon_path + 'monitoring-project-data.svg',
-      'organization_chart':       icon_path + 'organization-chart.svg',
-      'personal_desk':            icon_path + 'personal-desk.svg',
-      'personal_development':     icon_path + 'personal-development.svg',
-      'schedule_planning':        icon_path + 'schedule-planning.svg',
-      'startup_launch':           icon_path + 'startup-launch.svg',
-      'statistical_analysis':     icon_path + 'statistical-analysis.svg',
-      'success_mission':          icon_path + 'success-mission.svg',
-      'survey_list':              icon_path + 'survey-list.svg',
-      'take_a_break':             icon_path + 'take_a_break.svg',
-      'team_leader':              icon_path + 'team-leader.svg',
-      'winners_podium':           icon_path + 'winners-podium.svg'
-    }
+    def large_svg_icon(icon_name, options = {})
+      options.add_class('svg-icon-large')
+      svg_icon(icon_name, options)
+    end
+
+    def xl_svg_icon(icon_name, options = {})
+      options.add_class('svg-icon-xl')
+      svg_icon(icon_name, options)
+    end
+
+    def futuro_icon_for(i)
+      i.to_s if FuturoIcons.include? i.to_s
+    end
+
+    FuturoIcons = Dir.glob(Pano::Engine.root.to_s + '/app/assets/images/pano/icons/*.svg').collect{|icon| icon.match(/(?!\/)(\w+).(?=\.svg$)/)[0]}.sort
 
     MaterialIcons = {
       angle_up: 'keyboard_arrow_up',
