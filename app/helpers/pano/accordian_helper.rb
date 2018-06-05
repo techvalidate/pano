ecf0429510016c2ad6dfd515ffe5938af738d575
Matt Lindeboom <mattl@surveymonkey.com>
2018-06-05 13:54:57 -0700

    Adds accordian helper

---
+++ app/helpers/pano/accordian_helper.rb
@@ -0,0 +1,64 @@
+module Pano
+  module AccordianHelper
+
+    # Returns accordian component
+    #
+    # = accordian do |a|
+    #   a.item 'Item Label', open: true do
+    #     %p Item Content
+    # # =>  <ul class="accordian" data-controller="accordian">
+    # #       <li class="item item-open">
+    # #         <div class="item-label" data-action="click->accordian#toggle">
+    # #           Item Label
+    # #         </div>
+    # #         <div class="item-content">
+    # #           <p>Item Content</p>
+    # #         </div>
+    # #       <li>
+    def accordian(&block)
+      builder = AccordianBuilder.new(self)
+      content_tag :ul, class: 'accordian', data: {controller: 'accordian'} do
+        capture(builder, &Proc.new)
+      end
+    end
+
+    def accordian_item_label(&block)
+      capture(&block)
+    end
+
+
+    #
+    #
+    #
+    #
+    class AccordianBuilder
+      def initialize(template)
+        @template = template
+      end
+
+      def item(label = '', open: false, &block)
+        open_class = open ? 'item-open' : ''
+        title = item_label label
+        body = item_body do
+          yield
+        end
+
+        @template.content_tag :li, class: "item #{open_class}" do
+          title + body
+        end
+      end
+
+      private
+
+      def item_label(text = '')
+        @template.content_tag :div, text, class: 'item-label', data: {action: 'click->accordian#toggle'}
+      end
+
+      def item_body(&block)
+        @template.content_tag :div, class: 'item-content' do
+          block.call
+        end
+      end
+    end
+  end
+end
