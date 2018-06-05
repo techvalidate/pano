30665479041ecc43baaddba5e219c3850f8293ee
Matt Lindeboom <mattl@surveymonkey.com>
2018-06-05 13:55:39 -0700

    Correct comments

--- app/helpers/pano/accordian_helper.rb
+++ app/helpers/pano/accordian_helper.rb
@@ -3,18 +3,24 @@ module Pano
 
     # Returns accordian component
     #
-    # = accordian do |a|
-    #   a.item 'Item Label', open: true do
-    #     %p Item Content
-    # # =>  <ul class="accordian" data-controller="accordian">
-    # #       <li class="item item-open">
-    # #         <div class="item-label" data-action="click->accordian#toggle">
-    # #           Item Label
-    # #         </div>
-    # #         <div class="item-content">
-    # #           <p>Item Content</p>
-    # #         </div>
-    # #       <li>
+    #   = accordian do |a|
+    #     a.item 'Item Label', open: true do
+    #       %p Item Content
+    #   # =>  <ul class="accordian" data-controller="accordian">
+    #   #       <li class="item item-open">
+    #   #         <div class="item-label" data-action="click->accordian#toggle">
+    #   #           Item Label
+    #   #         </div>
+    #   #         <div class="item-content">
+    #   #           <p>Item Content</p>
+    #   #         </div>
+    #   #       <li>
+    #
+    # To easily pass an html label use +accordian_item_label+ helper
+    #
+    #   label = accordian_item_label do
+    #     %h3 Item Label
+    #   # => <div class="item-label"><h3>Item Label</h3></div>
     def accordian(&block)
       builder = AccordianBuilder.new(self)
       content_tag :ul, class: 'accordian', data: {controller: 'accordian'} do
@@ -26,11 +32,6 @@ module Pano
       capture(&block)
     end
 
-
-    #
-    #
-    #
-    #
     class AccordianBuilder
       def initialize(template)
         @template = template
