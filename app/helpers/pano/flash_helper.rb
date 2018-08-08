bda7760fc5eb38645ed7c7c540714a73fe16854c
Matt Lindeboom <mattl@surveymonkey.com>
2018-08-08 16:23:47 -0700

    Flash style updates

--- app/helpers/pano/flash_helper.rb
+++ app/helpers/pano/flash_helper.rb
@@ -4,13 +4,31 @@ module Pano
     def flash_messages
       content = ''
       flash.keys.each do |key|
+        :ruby
+          case key
+          when 'notice'
+            flash_title = 'Success'
+            flash_icon = :done
+          when 'alert' || 'warning' || 'error'
+            flash_title = 'Error'
+            flash_icon = :warning
+          when 'info'
+            flash_icon = :info
+          else
+            flash_icon = nil
+          end
+
         # behavior = 'hide-flash'
         # behavior += ' transient-flash' if [:notice, :transient].include? key.to_sym
-
         content +=
-          "<div class='flash flash-#{h key}'>
-            #{icon(:close, class: 'close-icon')}
-            #{h flash[key]}
+          "<div class='flash flash-#{h key}' data-controller='flash'>
+            #{icon(:close, class: 'close-icon', data: {action: 'click->flash#close'})}
+            <ul class='inline align-items-top'>
+              <li>#{icon flash_icon, class: 'flash-icon'}</li>
+              <li>
+                <p class='featured'>#{flash_title}</p>
+                <p class='mt-s'>#{h flash[key]}</p>
+              </li>
           </div>"
 
       end
