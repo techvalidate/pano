8e6e19a0030e321ecaef182c340751bb5703419d
Matt Lindeboom <mattl@surveymonkey.com>
2018-08-21 13:40:23 -0700

    Give date-input active attribute

--- app/models/pano/date_form_builder.rb
+++ app/models/pano/date_form_builder.rb
@@ -2,7 +2,7 @@ module Pano
   class DateFormBuilder < ActionView::Helpers::FormBuilder
     def formatted_date_field(name, options = {})
       hidden = @template.hidden_field(@object_name, name)
-      date = @template.tag('date-input', {name: "#{@object_name}[#{name}]", role: "textbox", value: @object.read_attribute(name) || @object.send(name), id: sanitize_to_id("#{@object_name}[#{name}]"), tabindex: 0}.update(options.stringify_keys))
+      date = @template.tag('date-input', {name: "#{@object_name}[#{name}]", role: "textbox", value: @object.read_attribute(name) || @object.send(name), id: sanitize_to_id("#{@object_name}[#{name}]"), tabindex: 0, active: false}.update(options.stringify_keys))
       @template.safe_join([date, hidden])
     end
 
