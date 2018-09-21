15eb22411d75b2a66e55fce2e9f1a1c7450adf53
Matt Lindeboom <mattl@surveymonkey.com>
2018-09-21 14:03:01 -0700

    Place hidden input inside of date-input

--- app/models/pano/date_form_builder.rb
+++ app/models/pano/date_form_builder.rb
@@ -1,9 +1,11 @@
 module Pano
   class DateFormBuilder < ActionView::Helpers::FormBuilder
     def formatted_date_field(name, options = {})
-      hidden = @template.hidden_field(@object_name, name)
-      date = @template.tag('date-input', {name: "#{@object_name}[#{name}]", role: "textbox", value: @object.read_attribute(name) || @object.send(name), id: sanitize_to_id("#{@object_name}[#{name}]"), tabindex: 0, active: false}.update(options.stringify_keys))
-      @template.safe_join([date, hidden])
+      hidden_field = @template.hidden_field(@object_name, name)
+
+      @template.content_tag('date-input', {name: "#{@object_name}[#{name}]", role: "textbox", value: @object.read_attribute(name) || @object.send(name), id: sanitize_to_id("#{@object_name}[#{name}]"), tabindex: 0, active: false}.update(options.stringify_keys)) do
+        hidden_field
+      end
     end
 
     def sanitize_to_id(name)
