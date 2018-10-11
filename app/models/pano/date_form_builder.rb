9f348c2b0dfd6d68c492a42c4a3667a07cd248f2
Matt Lindeboom <mattl@surveymonkey.com>
2018-10-11 09:16:17 -0700

    Date Input builder updates

--- app/models/pano/date_form_builder.rb
+++ app/models/pano/date_form_builder.rb
@@ -1,11 +1,10 @@
 module Pano
   class DateFormBuilder < ActionView::Helpers::FormBuilder
     def formatted_date_field(name, options = {})
-      hidden_field = @template.hidden_field(@object_name, name)
-
-      @template.content_tag('date-input', {name: "#{@object_name}[#{name}]", role: "textbox", value: @object.read_attribute(name) || @object.send(name), id: sanitize_to_id("#{@object_name}[#{name}]"), tabindex: 0, active: false}.update(options.stringify_keys)) do
-        hidden_field
-      end
+      hidden_field = @template.hidden_field(@object_name, name, data: {target: 'hidden'})
+      # date = @template.text_field_tag("#{@object_name}[#{name}]", '', {is: 'date-input', value: @object.read_attribute(name) || @object.send(name), id: sanitize_to_id("#{@object_name}[#{name}]"), tabindex: 0, active: false}.update(options.stringify_keys))
+      date = @template.tag('date-input', {name: "#{@object_name}[#{name}]", role: "textbox", value: @object.read_attribute(name) || @object.send(name), id: sanitize_to_id("#{@object_name}[#{name}]"), tabindex: 0, active: false}.update(options.stringify_keys))
+      @template.safe_join([hidden_field, date])
     end
 
     def sanitize_to_id(name)
