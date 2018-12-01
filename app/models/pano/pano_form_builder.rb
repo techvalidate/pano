76e84814c793e1b0a06fb160c84411bd9e75bd00
Ami Suzuki <amis@surveymonkey.com>
2018-11-30 17:18:34 -0800

    Fix SOLN-2062: IE11 calendar initial date population

--- app/models/pano/pano_form_builder.rb
+++ app/models/pano/pano_form_builder.rb
@@ -2,7 +2,8 @@ module Pano
   class PanoFormBuilder < ActionView::Helpers::FormBuilder
     def formatted_date_field(name, options = {})
       hidden_field = @template.hidden_field(@object_name, name, data: {target: 'hidden'})
-      date = @template.tag('date-input', {name: "#{@object_name}[#{name}]", role: "textbox", value: @object.read_attribute(name) || @object.send(name), id: sanitize_to_id("#{@object_name}[#{name}]"), tabindex: 0, active: false}.update(options.stringify_keys))
+      # Format value to be parseable by new Date() in IE11. Use to_time not to_date, to prevent date misalignment
+      date = @template.tag('date-input', {name: "#{@object_name}[#{name}]", role: "textbox", value: @object.read_attribute(name) ? @object.read_attribute(name).to_time.iso8601 : (@object.send(name) ? @object.send(name).to_time.iso8601 : @object.send(name)), id: sanitize_to_id("#{@object_name}[#{name}]"), tabindex: 0, active: false}.update(options.stringify_keys))
       @template.safe_join([hidden_field, date])
     end
 
