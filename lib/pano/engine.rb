946772862c890d3e302a751e7a1f6770349c4836
Matt Lindeboom <mattl@surveymonkey.com>
2018-12-21 12:31:50 -0800

    WiP

--- lib/pano/engine.rb
+++ lib/pano/engine.rb
@@ -20,6 +20,20 @@ module Pano
       )
     end
 
+    config.after_initialize do |app|
+      app.config.komponent.component_paths.append(Pano::Engine.root.join("frontend/components"))
+    end
+
+    initializer 'my_engine.action_dispatch' do |app|
+      ActiveSupport.on_load :action_controller do
+        ActionController::Base.prepend_view_path Pano::Engine.root.join("frontend")
+      end
+    end
+
+    initializer 'my_engine.autoload', before: :set_autoload_paths do |app|
+      app.config.autoload_paths << Pano::Engine.root.join('frontend')
+    end
+
     config.generators do |g|
       g.test_framework :rspec
     end
