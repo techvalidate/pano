4370340aa495ff7185abc4c7d8e6f07937c07451
Lou Capozzoli <louc@surveymonkey.com>
2019-02-28 09:38:42 -0800

    fixing some merge conflicts

--- spec/test_app/app/frontend/packs/wds.js
+++ spec/test_app/app/frontend/packs/wds.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-
-import 'components/'
-=======
 import 'components/index'
->>>>>>> pano-wds
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
 import application from 'stimulus_application'
 
 // customElements.define('date-input', DateInput, { extends: 'input' })
-<<<<<<< HEAD
 const wdsContext = require.context('components/wds/', true, /_controller\.js$/)
 const controllerContext = require.context('controllers', true, /_controller\.js$/)
 
@@ -19,8 +13,3 @@ var componentRequireContext = require.context("components/react/", true)
 var ReactRailsUJS = require("react_ujs")
 ReactRailsUJS.useContext(componentRequireContext)
 
-=======
-const context = require.context('components/wds/', true, /_controller\.js$/)
-console.log(definitionsFromContext(context))
-application.load(definitionsFromContext(context))
->>>>>>> pano-wds
