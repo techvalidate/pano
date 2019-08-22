8289004c47b7395c967843064ea24cad4a698488
Minh Reigen <minhr@surveymonkey.com>
2019-08-21 17:57:36 -0700

    Update regex based on @disaacs-sm 's feedback

--- app/models/pano/email_validator.rb
+++ app/models/pano/email_validator.rb
@@ -1,12 +1,12 @@
 module Pano
   class EmailValidator < ActiveModel::EachValidator
     # https://emailregex.com
-    EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i
+    EMAIL_REGEX = /\A(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])\z/i
 
     def validate_each(object,attribute,value)
       if value.blank?
         object.errors[attribute] << (options[:message] || "can't be blank") unless options[:allow_blank]
-      elsif !value.scan(EMAIL_REGEX) || value.scan(EMAIL_REGEX).size != 1 || value.scan(EMAIL_REGEX).first.size != value.size
+      elsif value.scan(EMAIL_REGEX).empty?
         object.errors[attribute] << (options[:message] || 'is not a valid email address')
       end
     end
